1c8d3d168a24950c45e9f4f0be80160b696dff16
tpereira <tpereira@student.42lisboa.com>
2022-12-28 12:50:31 +0000

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 12:36:45 by tpereira          #+#    #+#             */
+/*   Updated: 2022/12/28 12:45:31 by tpereira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+					argv[i][j] -= 32;
+				j++;
+			}
+			std::cout << argv[i];
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
