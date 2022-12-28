e63827782dba521a6709982beb4c10d54c487a2f
tpereira <tpereira@student.42lisboa.com>
2022-12-28 14:41:20 +0000

    save

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 12:36:45 by tpereira          #+#    #+#             */
-/*   Updated: 2022/12/28 14:39:26 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/12/28 12:55:23 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,5 @@
 
 int main(int argc, char **argv)
 {
-	int	i;
-	int	j;
-
-	if (argc < 2)
-		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		i = 1;
-		while (i < argc)
-		{
-			j = 0;
-			while (argv[i][j])
-			{
-				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-					argv[i][j] -= 32;
-				std:: cout << argv[i][j];
-				j++;
-			}
-			i++;
-		}
-		std:: cout << std::endl;
-	}	
+	
 }
